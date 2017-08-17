a34bedc190e5d839d1f34f713ec71cef56bb57a9
shweyeewinmaung <shweyeewinmaung@gmail.com>
2017-08-17 10:35:06 +0630

    add

--- address.sql
+++ address.sql
@@ -1,27 +1,33 @@
--- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: addressdb
--- ------------------------------------------------------
--- Server version	5.5.5-10.1.25-MariaDB
+-- Host: 127.0.0.1
+-- Generation Time: Aug 17, 2017 at 05:57 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `addressdb`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_addressbook`
 --
 
-DROP TABLE IF EXISTS `tbl_addressbook`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_addressbook` (
   `AddressBook_ID` varchar(50) CHARACTER SET utf8 NOT NULL,
   `AddressBook_Name` varchar(255) CHARACTER SET utf8 NOT NULL,
@@ -29,50 +35,55 @@ CREATE TABLE `tbl_addressbook` (
   `AddressBook_Email` varchar(50) CHARACTER SET utf8 NOT NULL,
   `AddressBook_Address` text CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tbl_addressbook`
 --
 
-LOCK TABLES `tbl_addressbook` WRITE;
-/*!40000 ALTER TABLE `tbl_addressbook` DISABLE KEYS */;
-INSERT INTO `tbl_addressbook` VALUES ('A-000001','aa',444,'aa@gmail.com','fdafser'),('A-000002','bb222',34234,'bb@gmail.com','fdaefw'),('A-000003','cc',555,'bb33@gmail.com','daefw');
-/*!40000 ALTER TABLE `tbl_addressbook` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `tbl_addressbook` (`AddressBook_ID`, `AddressBook_Name`, `AddressBook_Phone`, `AddressBook_Email`, `AddressBook_Address`) VALUES
+('A-000001', 'aa', 444, 'aa@gmail.com', 'fdafser'),
+('A-000002', 'bb222', 34234, 'bb@gmail.com', 'fdaefw'),
+('A-000003', 'cc', 555, 'bb33@gmail.com', 'daefw');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_user`
 --
 
-DROP TABLE IF EXISTS `tbl_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tbl_user` (
-  `User_ID` int(50) NOT NULL AUTO_INCREMENT,
+  `User_ID` int(50) NOT NULL,
   `User_Name` varchar(255) NOT NULL,
-  `User_Password` varchar(50) NOT NULL,
-  PRIMARY KEY (`User_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `User_Password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_user`
 --
 
-LOCK TABLES `tbl_user` WRITE;
-/*!40000 ALTER TABLE `tbl_user` DISABLE KEYS */;
-INSERT INTO `tbl_user` VALUES (1,'aa','aa');
-/*!40000 ALTER TABLE `tbl_user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `tbl_user` (`User_ID`, `User_Name`, `User_Password`) VALUES
+(1, 'aa', 'aa');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `User_ID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-08-16 16:31:51
